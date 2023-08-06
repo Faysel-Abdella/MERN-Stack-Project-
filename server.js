295a9642c73a536a245e4a226ff4ae6c72f14ac8
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-06 22:13:25 +0100

    morgan and dotenv is added with some adjustment

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
@@ -6,15 +10,23 @@ const app = express();
 // as just a javascript object
 app.use(express.json());
 
+//If i am not in development phase (if i am in production phase), i don't
+//to log any request deatils(only if i'm developing i want to log details)
+if (process.env.NODE_ENV === "development") {
+  //When you deploy this app the deployment platform will change the NODE_ENV to
+  //"production" so the following line will not excute
+  app.use(morgan("dev"));
+}
+
 app.get("/", (req, res, next) => {
   res.send("Hi world");
 });
 
 app.post("/", (req, res, next) => {
-  console.log(req);
+  // console.log(req);
   res.json({ message: "Data received", data: req.body });
 });
 
-app.listen(5100, () => {
+app.listen(process.env.PORT || 5100, () => {
   console.log("server start");
 });
