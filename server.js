7441e45d73d895dbb685de563c28c47c616f6ee2
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-06 21:22:53 +0100

    express.json() middleware is added to parse all incomming POST or PUT requests.

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
+// Parse any incomming json POST or PUT request and enable working with it
+// as just a javascript object
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
   res.send("Hi world");
+});
+
+app.post("/", (req, res, next) => {
+  console.log(req);
+  res.json({ message: "Data received", data: req.body });
 });
 
 app.listen(5100, () => {
