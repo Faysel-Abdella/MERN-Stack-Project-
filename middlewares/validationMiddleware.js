af3c013e10b4a95d469dec1432fe4295f5402ee0
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-07 22:34:52 +0100

    validate register input middleware is created and added to register route.

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,7 +1,8 @@
 import { body, param, validationResult } from "express-validator";
 import { StatusCodes } from "http-status-codes";
 import { JOB_TYPE, JOB_STATUS } from "../util/constants.js";
-import mongoose from "mongoose";
+import User from "../models/userModel.js";
+import mongoose, { Error } from "mongoose";
 
 //This middleware contains validate test and the function for error form validating
 
@@ -17,6 +18,9 @@ const withValidatorErrors = (validateValues) => {
         const errorMessages = errorArray.map((error) => error.msg);
         const error = new Error(errorMessages);
         error.statusCode = StatusCodes.BAD_REQUEST;
+        if (errorMessages[0].startsWith("no job")) {
+          error.statusCode = StatusCodes.NOT_FOUND;
+        }
         throw error;
       }
       next();
@@ -45,6 +49,29 @@ export const validateIdParam = withValidatorErrors([
       return mongoose.Types.ObjectId.isValid(value);
     })
     .withMessage("invalid MongoDB id"),
+]);
+
+export const validateRegisterInput = withValidatorErrors([
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    //check if there is a user with the same email
+    .custom(async (value) => {
+      const user = await User.findOne({ email: value });
+      if (user) {
+        throw new Error("email already exist");
+      }
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 5, max: 30 })
+    .withMessage("password must be at least 5 character"),
+  body("lastName").notEmpty().withMessage("Last name is required"),
+  body("location").notEmpty().withMessage("Location is required"),
 ]);
 
 //Now in all routes i will add these exporting function by passing my own testing logic
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,8 +3,9 @@ import express from "express";
 const router = express.Router();
 
 import { register, login } from "../controllers/authController.js";
+import { validateRegisterInput } from "../middlewares/validationMiddleware.js";
 
-router.post("/register", register);
+router.post("/register", validateRegisterInput, register);
 router.post("/login", login);
 
 export default router;
