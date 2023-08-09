df6fd0052ace84afa0ed0145913815bada402c31
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-09 21:46:57 +0100

    useNavigation is used to update the text based on the navigation state.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { Form, Link, redirect } from "react-router-dom";
+import { Form, Link, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../util/customFetch.js";
@@ -13,11 +13,15 @@ export const action = async ({ request }) => {
     await customFetch.post("/auth/register", data);
     return redirect("/login");
   } catch (error) {
+    console.log(error);
     return error;
   }
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  // console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -33,8 +37,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="earth" />
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Submit"}
         </button>
         <p>
           Already a member?
