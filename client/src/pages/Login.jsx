d609f307e16f8987eeb46b63f39e3e0a0e58f396
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-09 22:58:26 +0100

    useActionData is used to display error message on the component based on the returned value of action

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,10 @@
-import { Link, Form, useNavigation, redirect } from "react-router-dom";
+import {
+  Link,
+  Form,
+  useNavigation,
+  redirect,
+  useActionData,
+} from "react-router-dom";
 import { toast } from "react-toastify";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
@@ -7,13 +13,19 @@ import customFetch from "../util/customFetch.js";
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  const errors = { message: "" };
+  if (data.password.length < 3) {
+    errors.message = "password too short";
+    return errors;
+  }
 
   try {
     await customFetch.post("/auth/login", data);
     toast.success("Login success");
     return redirect("/dashboard");
   } catch (error) {
-    toast.error(error?.response?.data?.message);
+    // toast.error(error?.response?.data?.message);
+    error.message = error?.response?.data?.message;
     return error;
   }
 };
@@ -21,10 +33,16 @@ export const action = async ({ request }) => {
 const LogIn = () => {
   const navigation = useNavigation();
   const isSubmitting = navigation === "submitting";
+
+  // using the data returned from the action
+  const errors = useActionData();
+  //If the the action did not return any value this will be undefined
   return (
     <Wrapper>
       <Form method="post" className="form">
         <Logo />
+        {errors?.message && <p style={{ color: "red" }}>{errors.message}</p>}
+        <p></p>
         <h4>Login</h4>
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
