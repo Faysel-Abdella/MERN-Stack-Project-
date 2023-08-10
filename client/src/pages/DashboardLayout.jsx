02c7075042c4fb19ac554ab60838ad31327584f2
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-10 18:25:08 +0100

    loader is used to get the current user.

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,12 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, redirect, useLoaderData } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSideBar, SmallSideBar, Navbar } from "../components";
 import { createContext, useContext, useState } from "react";
+import customFetch from "../util/customFetch.js";
 
 const DashbordContext = createContext();
 
+export const loader = async () => {
+  try {
+    //Make request to the current user
+    const res = await customFetch.get("/users/current-user");
+    //extract the data response from axios response
+    const { data } = res;
+    //return it to use it in the component
+    return data;
+  } catch (error) {
+    // If there is any error with finding the user when the user go to dashboard just redirect it to '/'
+    return redirect("/");
+  }
+};
+
 const DashboardLayout = ({ isDarkThemeEnabled }) => {
-  const user = { name: "john" };
+  //Extract the user from the returned loader function
+  const { user } = useLoaderData();
+  console.log(user);
+
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(isDarkThemeEnabled);
 
