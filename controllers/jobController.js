79e6d3db90efbbaaf26b8cc4ec1522fe006559fd
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-08-13 03:10:48 +0100

    Group by Monthly Applications

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -80,26 +80,51 @@ export const showStats = async (req, res, next) => {
     return acc;
   }, {});
 
-  console.log(stats);
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
 
-  const defaultStats = {
-    pending: stats.pending || 0,
-    interview: stats.interview || 0,
-    declined: stats.declined || 0,
-  };
-  let monthlyApplications = [
-    {
-      date: "May 23",
-      count: 12,
-    },
-    {
-      date: "Jun 23",
-      count: 9,
-    },
-    {
-      date: "Jul 23",
-      count: 3,
-    },
-  ];
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format("MMM YY");
+      return { date, count };
+    })
+    .reverse();
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
+
+// const defaultStats = {
+//   pending: stats.pending || 0,
+//   interview: stats.interview || 0,
+//   declined: stats.declined || 0,
+// };
+// let monthlyApplications = [
+//   {
+//     date: "May 23",
+//     count: 12,
+//   },
+//   {
+//     date: "Jun 23",
+//     count: 9,
+//   },
+//   {
+//     date: "Jul 23",
+//     count: 3,
+//   },
+// ];
